558b9475ec0d09d8c938e6d6d11c6997170af6f5
Francesco Giannice <giannice.developer@gmail.com>
2025-04-04 18:51:55 +0200

    Added void expressions

--- mussel/src/main.rs
+++ mussel/src/main.rs
@@ -1,5 +1,6 @@
 mod parser; // Links the 'parser' module, providing access to its parsing functions and types.
 mod interpreter; // Links the 'interpreter' module, enabling the execution of parsed expressions.
+use std::collections::HashMap;
 
 fn main() {
     // Reads the contents of the file 'hello.mus' at compile time and includes it as a string
@@ -7,8 +8,10 @@ fn main() {
 
     // Parses the file content into a vector of expressions using 'parse_expr'.
     // The 'unwrap()' method is used to extract the parsing result, assuming it succeeds.
-    let (_, expr) = parser::parse_expr(input).unwrap();
+    let (_, exprs) = parser::parser(input).unwrap();
 
-    // Prints the parsed expressions for debugging purposes.
-    dbg!(&expr);
+    let mut context = HashMap::new();
+    for expr in exprs {
+        interpreter::interpreter(expr, &mut context);
+    }
 }
