44ccdf37b9ad645d4fdc1d7af9e441aba96a7f70
MatyasZsombor <mzsombi@icloud.com>
2025-04-21 16:17:33 +0200

    Added new string functions
    Added split, reverse, trim, ltrim, rtrim

--- mussel/src/stdlib/string.rs
+++ mussel/src/stdlib/string.rs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 use crate::parser::{Atom, Expr};
+use core::panic;
 use std::collections::HashMap;
 
 // This function will be called when the user writes "include string"
@@ -21,6 +22,11 @@ pub fn load(context: &mut HashMap<String, Expr>) {
     context.insert("lowercase".to_string(), Expr::Builtin(string_lowercase));
     context.insert("uppercase".to_string(), Expr::Builtin(string_uppercase));
     context.insert("length".to_string(), Expr::Builtin(string_length));
+    context.insert("split".to_string(), Expr::Builtin(string_split));
+    context.insert("reverse".to_string(), Expr::Builtin(string_reverse));
+    context.insert("trim".to_string(), Expr::Builtin(string_trim));
+    context.insert("ltrim".to_string(), Expr::Builtin(string_ltrim));
+    context.insert("rtrim".to_string(), Expr::Builtin(string_rtrim));
 }
 
 // Convert a string to lowercase
@@ -29,9 +35,7 @@ pub fn string_lowercase(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -
         panic!("lowercase expects 1 argument: a string");
     }
     match &args[0] {
-        Expr::Constant(Atom::String(s)) => {
-            Expr::Constant(Atom::String(s.to_lowercase()))
-        }
+        Expr::Constant(Atom::String(s)) => Expr::Constant(Atom::String(s.to_lowercase())),
         _ => panic!("lowercase expects a string argument"),
     }
 }
@@ -42,9 +46,7 @@ pub fn string_uppercase(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -
         panic!("uppercase expects 1 argument: a string");
     }
     match &args[0] {
-        Expr::Constant(Atom::String(s)) => {
-            Expr::Constant(Atom::String(s.to_uppercase()))
-        }
+        Expr::Constant(Atom::String(s)) => Expr::Constant(Atom::String(s.to_uppercase())),
         _ => panic!("uppercase expects a string argument"),
     }
 }
@@ -55,9 +57,68 @@ pub fn string_length(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -> E
         panic!("length expects 1 argument: a string");
     }
     match &args[0] {
-        Expr::Constant(Atom::String(s)) => {
-            Expr::Constant(Atom::Number(s.len() as i64))
-        }
+        Expr::Constant(Atom::String(s)) => Expr::Constant(Atom::Number(s.len() as i64)),
         _ => panic!("length expects a string argument"),
+    }
+}
+
+// Split a string by another
+pub fn string_split(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -> Expr {
+    if args.len() != 2 {
+        panic!("split expects 2 arguments: strings");
+    }
+    match (&args[0], &args[1]) {
+        (Expr::Constant(Atom::String(s1)), Expr::Constant(Atom::String(s2))) => Expr::Array(
+            s1.split(s2.as_str())
+                .map(|s| Expr::Constant(Atom::String(s.to_string())))
+                .collect(),
+        ),
+        _ => panic!("split expects strings as arguments"),
+    }
+}
+
+// Reverse a string
+pub fn string_reverse(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -> Expr {
+    if args.len() != 1 {
+        panic!("reverse expects 1 argument: a string");
+    }
+    match &args[0] {
+        Expr::Constant(Atom::String(s)) => {
+            Expr::Constant(Atom::String(s.as_str().chars().rev().collect()))
+        }
+        _ => panic!("reverse expects a string argument"),
+    }
+}
+
+//Remove whitespace from both ends
+pub fn string_trim(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -> Expr {
+    if args.len() != 1 {
+        panic!("trim expects 1 argument: a string");
+    }
+    match &args[0] {
+        Expr::Constant(Atom::String(s)) => Expr::Constant(Atom::String(s.trim().to_string())),
+        _ => panic!("trim expects a string as argument"),
+    }
+}
+
+//Remove leading whitespace
+pub fn string_ltrim(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -> Expr {
+    if args.len() != 1 {
+        panic!("ltrim expects 1 argument: a string");
+    }
+    match &args[0] {
+        Expr::Constant(Atom::String(s)) => Expr::Constant(Atom::String(s.trim_start().to_string())),
+        _ => panic!("ltrim expects a string argument"),
+    }
+}
+
+//Remove trailing whitespace
+pub fn string_rtrim(args: Vec<Expr>, _context: &mut HashMap<String, Expr>) -> Expr {
+    if args.len() != 1 {
+        panic!("rtrim expects 1 argument: a string");
+    }
+    match &args[0] {
+        Expr::Constant(Atom::String(s)) => Expr::Constant(Atom::String(s.trim_end().to_string())),
+        _ => panic!("rtrim expects a string argument"),
     }
 }
